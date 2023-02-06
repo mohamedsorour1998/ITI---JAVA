7f563edf53ad55af12d8df5d83936bb3ec2596f2
mohamedsorour1998 <mohamedsorour1998@gmail.com>
2023-02-06 15:10:03 +0200

    Finished TASK1! Create an object from BiPredicate interface using lambda

---
+++ LAB5/TASK1/StringUtils.java
@@ -0,0 +1,24 @@
+/**
+ * StringUtils
+ */
+public class StringUtils {
+    public static String betteString(String s1, String s2, BiPredicate<String, String> p) {
+        if (p.test(s1, s2)) {
+            return s1;
+        } else {
+            return s1;
+        }
+    }
+
+    public static void main(String[] args) {
+        String string1 = new String("Hello");
+        String string2 = new String("Welcome");
+        // create an object from BiPredicate interface using lambda
+        BiPredicate<String, String> p1 = (s1, s2) -> s1.length() > s2.length();
+        BiPredicate<String, String> p2 = (s1, s2) -> true;
+        System.out.println(
+                betteString(string1, string2, p1));
+        System.out.println(
+                betteString(string1, string2, p2));
+    }
+}
